fb875a218838475cb1844ba1f3d2008bffd1fc7d
ningoy <ouyang_ning@126.com>
2021-10-15 20:04:01 +0800

    *feature) 1.前端列表页。

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
+
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  el: '#app',
+  template: '<App/>',
+  components: { App }
 }).$mount('#app')
